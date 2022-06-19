72009b9a5e1557d83b30efe622e15fc3a7538e0b
Lee-moo <dlandif22@gmail.com>
2022-06-19 13:33:01 +0900

    completed problem 3062

--- src/string/problem3062/Main.java
+++ src/string/problem3062/Main.java
@@ -1,23 +1,32 @@
 package string.problem3062;
 
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        for (int i = 0; i < sc.nextInt(); i++) {
-            String input = sc.nextLine();
-            input = String.valueOf(Integer.parseInt(input) + Integer.parseInt(new StringBuffer(input).reverse().toString()));
-            boolean res = true;
-            char[] arr = input.toCharArray();
+    private static int tc;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        tc = Integer.parseInt(st.nextToken());
+        String num = null;
+        for (int i = 0; i < tc; i++) {
+            st = new StringTokenizer(br.readLine());
+            num = st.nextToken();
+            num = String.valueOf(Integer.parseInt(num) + Integer.parseInt(new StringBuffer(num).reverse().toString()));
+            char[] arr = num.toCharArray();
+            boolean ans = true;
             for (int j = 0; j < arr.length; j++) {
-                System.out.println("hi");
-                if (arr[j] != arr[arr.length - j - 1]) {
-                    res = false;
+                if (arr[j] != arr[arr.length- j - 1]) {
+                    ans = false;
                     break;
                 }
             }
-            if(res) System.out.println("YES");
+            if(ans) System.out.println("YES");
             else System.out.println("NO");
 
         }
